2270520f60e9e426ca5e8248599aad52f2c8a3ff
Mirco <Mirco@192.168.0.120>
2014-08-20 17:26:44 +0200

    client udp2 written

---
+++ src/networking/client_udp2.py
@@ -0,0 +1,18 @@
+'''
+Created on 20.08.2014
+
+@author: Mirco
+'''
+
+import socket
+
+ip = "localhost"
+soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+try:
+    while True:
+        message = input("Enter new message: ")
+        if(message == "exit"):
+            break
+        soc.sendto(message.encode(encoding='utf_8', errors='strict'), (ip, 60000))
+finally:
+    soc.close()
--- src/networking/server_udp.py
+++ src/networking/server_udp.py
@@ -8,7 +8,7 @@ import socket
 
 soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
-    soc.bind(("", 60000))
+    soc.bind(("192.168.0.120", 60000))
     while True:
         (daten, address) = soc.recvfrom(1024)
         print("[%s] %s" % (address[0], daten))
